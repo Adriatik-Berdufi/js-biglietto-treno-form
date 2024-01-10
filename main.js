dfe4d3ac7987626a16247f9b1977d29329fff3ca
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-10 19:05:23 +0100

    feat:add controllo inserimento dati

--- main.js
+++ main.js
@@ -12,79 +12,42 @@ let costo;
 
 
 
+
+
+
+
 compra.addEventListener('click' ,function(){
+    
+    
     const usarname = nameBiglietto.value ;
     resultName.innerHTML = usarname; 
 
-    const km = parseInt(kmDaPercorrere.value)
-
-     //calocolo costo
-    if(fasciaEta.value == "minorenne"){
-        costo = km * prezzoPerKm * 0.8;
-        resultPrice.innerHTML = costo + " $";  
-        
-    }else if(fasciaEta.value == "over"){
-        costo = km * prezzoPerKm * 0.6;
-        resultPrice.innerHTML = costo + " $";
-        
+    const km = kmDaPercorrere.value
+    if(usarname == "" || isNaN(km) || km == "" ){
+        alert("Dati Mancanti o Errati");
     }else{
-        costo =  km * prezzoPerKm;
-        resultPrice.innerHTML = costo + " $" ;
-    
+
+            //calocolo costo
+        if(fasciaEta.value == "minorenne"){
+            costo = km * prezzoPerKm * 0.8;
+            resultPrice.innerHTML = costo + " $";  
+            
+        }else if(fasciaEta.value == "over"){
+            costo = km * prezzoPerKm * 0.6;
+            resultPrice.innerHTML = costo + " $";
+            
+        }else{
+            costo =  km * prezzoPerKm;
+            resultPrice.innerHTML = costo + " $" ;
+        
+        }
+        
+        biglietto.classList.remove("d-none");
+         
     }
+   
 
-    biglietto.classList.remove("d-none");
-
+    
 
 
 } );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-let prezzo;
-let prezzoFinale = document.getElementById("prezzofinale") ;
-
-
-
-
-
-
-
-if(isNaN(userEta) || isNaN(kmDaPerCorrere)){
-    error = true;
-    prezzoFinale.innerText = "i dati insseriti non son correti. RIPROVA " ;
-}else{
-    error = false;
-}
-
-if(!error){
-    if (userEta < 18){
-        prezzo = (kmDaPerCorrere * 0.8 * 0.21).toFixed(2);
-        console.log(prezzo);
-    }else if (userEta >= 65){
-        prezzo = (kmDaPerCorrere * 0.6 * 0.21).toFixed(2);
-        console.log(prezzo);
-    }else{
-        prezzo = (kmDaPerCorrere * 0.21).toFixed(2);
-        console.log(prezzo);
-    }
-    prezzoFinale.innerText = "il prezzo del biglietto e: " +  prezzo + "$";
-}*/
