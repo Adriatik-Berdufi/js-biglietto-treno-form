a68c20032e75210c5014e076094cafba52a2ca52
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-10 18:17:18 +0100

    feat: add logic buy

--- main.js
+++ main.js
@@ -1,12 +1,71 @@
-/*
-let kmDaPerCorrere = parseInt (prompt('Numero di chilometri che vuole percorrere?'));
-console.log(kmDaPerCorrere);
-let userEta = parseInt (prompt('Inserisci la tua eta'));
-console.log(userEta);
+const nameBiglietto = document.getElementById("nameBiglietto");
+const kmDaPercorrere = document.getElementById("kmDaPercorrere");
+const fasciaEta = document.getElementById("fasciaEta");
+const compra = document.getElementById("copra");
+
+const resultName = document.getElementById("nameBigliettoComprato");
+const resultPrice = document.getElementById("costo");
+
+
 const prezzoPerKm = 0.21;
-console.log(prezzoPerKm);
+let costo;
+
+
+
+compra.addEventListener('click' ,function(){
+    const usarname = nameBiglietto.value ;
+    resultName.innerHTML = usarname; 
+
+    const km = parseInt(kmDaPercorrere.value)
+
+     //calocolo costo
+    if(fasciaEta.value == "minorenne"){
+        costo = km * prezzoPerKm * 0.8;
+        resultPrice.innerHTML = costo;  
+        
+    }else if(fasciaEta.value == "over"){
+        costo = km * prezzoPerKm * 0.6;
+        resultPrice.innerHTML = costo;
+        
+    }else{
+        costo =  km * prezzoPerKm;
+        resultPrice.innerHTML = costo;
+    
+    }
+
+   
+
+
+
+} );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let prezzo;
 let prezzoFinale = document.getElementById("prezzofinale") ;
+
+
+
+
+
 
 
 if(isNaN(userEta) || isNaN(kmDaPerCorrere)){
